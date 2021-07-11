850b72f079b5b94ec822b07eaf424d69d64713f0
amanda=posey <amanda.posey@live.com>
2021-07-10 21:14:28 -0400

    header created

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+<div className="header-wrapper">
+        <div className="main-info">
+            <h1>Web development and websites</h1>
+        </div>
+</div>
+    )
+}
+
+export default Header
