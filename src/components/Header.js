5830d17a45b8532808ddd15372d351cb4edf87ef
amanda=posey <amanda.posey@live.com>
2021-07-10 22:41:53 -0400

    header complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Typed from 'react-typed';
 
 const Header = () => {
     return (
 <div className="header-wrapper">
         <div className="main-info">
             <h1>Web development and websites</h1>
+            <Typed 
+                className='typed-text'
+                strings={['Web Design', 'Debugging', 'Web Development', 'Comic Relief']}
+                typeSpeed={40}
+                backSpeed={60}
+                loop
+            />
+            <a href='#' className='btn-main-offer'>Contact Me</a>
         </div>
 </div>
     )
