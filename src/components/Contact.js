dce915586974938a238cc95a1b4bcc7cda3e8c67
amanda=posey <amanda.posey@live.com>
2021-07-15 15:16:35 -0400

    boxes clickable

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,19 +10,23 @@ const Contact = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col-md-3 col-sm-6">
+                            <a href='https://www.linkedin.com/in/amanda-posey-software-engineer/'>
                             <div className="box">
                             <div className="circle"><FontAwesomeIcon className='icon' icon={faLinkedin} size='2x'/></div>
-                                <a className='link' href='https://www.linkedin.com/in/amanda-posey-software-engineer/'>LinkedIn</a>
+                                <h2>LinkedIn</h2>
                                 <p>Let's connect soon!</p>
                             </div>
+                            </a>
                         </div>
                         {/* - */}
                         <div className="col-md-3 col-sm-6">
+                            <a href='https://github.com/amanda-posey'>
                             <div className="box">
                             <div className="circle"><FontAwesomeIcon className='icon' icon={faGithub} size='2x'/></div>
-                                <a className='link' href='https://github.com/amanda-posey'>GitHub</a>
+                                <h2>GitHub</h2>
                                 <p>Have a look at my past projects.</p>
                             </div>
+                            </a>
                         </div>
                     </div>
                 </div> 
